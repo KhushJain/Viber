69bbb1181547433f68d346362d241e07072c536b
KhushJain <khush4080@gmail.com>
2019-12-15 21:42:25 +0530

    Made changes and added new files

--- music/urls.py
+++ music/urls.py
@@ -6,4 +6,6 @@ app_name = 'music'
 urlpatterns = [
     path('',views.index, name="index"),
     re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    path('register/',views.UserFormView.as_view(), name='register'),
+    path('login/',views.login_user, name='login_user'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
+from .forms import UserForm
 from .models import Album
 
 def index(request):
@@ -8,3 +12,47 @@ def index(request):
 def detail(request,album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, "music/detail.html", {"album":album})
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self,request):
+        form = self.form_class(None)
+        return render(request, self.template_name,{'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+        return render(request, self.template_name,{'form':form})
+
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('music:index')
+            else:
+                return render(request, 'music/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'music/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'music/login.html')
